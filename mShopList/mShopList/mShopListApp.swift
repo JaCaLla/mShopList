2a85e1d39e18d074fcba0d44debcc6cfc7455530
Javier Calatrava <javi.calatrava@gmail.com>
2022-10-23 20:18:30 +0200

    [feature] Created blank project

---
+++ mShopList/mShopList/mShopListApp.swift
@@ -0,0 +1,17 @@
+//
+//  mShopListApp.swift
+//  mShopList
+//
+//  Created by Javier Calatrava on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct mShopListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
